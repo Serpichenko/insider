ac8919afe74a7aff552037a747d2b81d00f9ae93
Fongolcs <fongolcs@gmail.com>
2021-03-18 21:30:16 +0200

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,13 @@ import { AppService } from './app.service';
 import { UserService } from './user/user.service';
 import { UserController } from './user/user.controller';
 import { UserModule } from './user/user.module';
+import { SubscriberModule } from './subscriber/subscriber.module';
+import { AccountModule } from './account/account.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(), UserModule],
+  imports: [TypeOrmModule.forRoot(), UserModule, SubscriberModule, AccountModule, AuthModule, UsersModule],
   controllers: [AppController, UserController],
   providers: [AppService, UserService],
 })
