1e5af95a670ee5c6f4cf41ed56852dec987662ac
Fongolcs <fongolcs@gmail.com>
2021-03-24 00:49:53 +0200

    upd

--- src/account/entities/account.entity.ts
+++ src/account/entities/account.entity.ts
@@ -1,1 +1,23 @@
-export class Account {}
+import { Entity, Column, PrimaryGeneratedColumn, ManyToOne } from 'typeorm';
+import { User } from '../../user/entities/user.entity';
+
+@Entity()
+export class Account {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  userId: number;
+
+  @Column()
+  totalYearSubs: number;
+
+  @Column()
+  totalMonthSubs: number;
+
+  @Column({ type: 'bigint' })
+  balance: string;
+
+  @ManyToOne(() => User, (user) => user.subscribers)
+  user: User;
+}
